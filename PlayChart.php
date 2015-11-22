9e39c965e6fab40e8349f1c56383063637cae60f
idiosyncraticee <chris.schuermyer@gmail.com>
2015-11-22 14:02:38 -0800

    Php query

---
+++ PlayChart.php
@@ -0,0 +1,34 @@
+<?php
+
+//$appid = "com.venmo";
+$appid = htmlspecialchars($_GET["appid"]);
+$category = htmlspecialchars($_GET["category"]);
+$collection = htmlspecialchars($_GET["collection"]);
+
+$pdo = new PDO('sqlite:playstore.db');
+
+
+$stmt = $pdo->prepare('SELECT date,rank FROM rank_data WHERE appid=:appid AND category=:category AND collection=:collection ORDER BY date ASC');
+//$stmt = $pdo->prepare('SELECT date,rank FROM rank_data WHERE appid = :appid');
+$stmt->execute(array('appid' => $appid, 'category' => $category, 'collection' => $collection));
+
+$datapie = array();
+
+//$result->setFetchMode(PDO::FETCH_ASSOC);
+
+foreach ($stmt as $row) {
+    // do something with $row
+    extract($row);
+
+    $datapie[] = array($date, $rank);
+}
+
+
+
+$data = json_encode($datapie);
+header('Content-type: application/json');
+//echo "Content-type: application/json\n";
+//echo "\n";
+echo $data;
+//echo "\n";
+?>
